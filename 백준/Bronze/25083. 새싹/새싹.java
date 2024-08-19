cd58678d6becbee4c67c64c6dbc5fd5bd95c3da2
BongSeok Sin <Stupic0713@gmail.com>
2024-08-19 10:26:12 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+
+}
