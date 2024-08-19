e8e2205f3dbd870561c388af9e0f23b6feda5643
BongSeok Sin <Stupic0713@gmail.com>
2024-08-19 10:23:15 +0900

    [Bronze V] Title: 오늘 날짜, Time: 164 ms, Memory: 16980 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+
+    }
+
+}
