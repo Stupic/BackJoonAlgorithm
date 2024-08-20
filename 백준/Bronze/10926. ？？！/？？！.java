7763783aed77200b123f0e6350381ad92d2b784d
BongSeok Sin <Stupic0713@gmail.com>
2024-08-20 11:04:41 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        System.out.println(str + "??!");
+
+    }
+}
