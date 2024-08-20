f3901c1e6bbad69043124e9f126dad3af72157a5
BongSeok Sin <Stupic0713@gmail.com>
2024-08-20 11:10:56 +0900

    [Bronze V] Title: 고양이, Time: 168 ms, Memory: 4452 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Backjoon
+{
+    class P10171
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+          
+        }
+       
+    }
+}
