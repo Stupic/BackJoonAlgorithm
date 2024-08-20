db7da97d1ec8aec4e327ff5806c61f5d44b84389
BongSeok Sin <Stupic0713@gmail.com>
2024-08-20 10:09:20 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main{
+   public static void main (String[] args) throws IOException{
+       System.out.print("Hello World!");
+       
+   } 
+}
