06fc4cb0e80b36e930a18fb7c6ac7794f01f356c
BongSeok Sin <Stupic0713@gmail.com>
2024-08-20 11:04:31 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 4664 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Security.Cryptography;
+
+namespace Backjoon
+{
+    class P11654
+    {
+        static void Main(String[] args)
+        {
+            int a;
+            a = Convert.ToInt32(Console.Read());
+
+            Console.WriteLine(a); 
+
+        }
+
+    }
+}
