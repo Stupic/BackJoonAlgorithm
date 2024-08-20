b4ef45278e8b790c05638d858a6ed222b84325b7
BongSeok Sin <Stupic0713@gmail.com>
2024-08-20 10:09:29 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    
+    int a;
+    int sum = 0;    
+
+    scanf("%d",&a);
+    sum = sum+a;
+    
+    scanf("%d",&a);
+    sum = sum+a;
+    
+    printf("%d",sum);
+    
+}
