9c35201498d455706aed925d9cf5b62f23566417
BongSeok Sin <Stupic0713@gmail.com>
2024-08-22 10:06:05 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 16292 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,45 +5,39 @@ import java.util.HashMap;
 
 public class Main {
     static HashMap<Integer, Fibonacci> fibonacciStore = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
-
-        Fibonacci initZero = new Fibonacci();
-        initZero.setZero(1);
-        fibonacciStore.put(0, initZero);
-        Fibonacci initOne = new Fibonacci();
-        initOne.setOne(1);
-        fibonacciStore.put(1, initOne);
-
-        int maxKey = 1;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        for(int i = 0 ; i < t ; i++){
+        for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-
-            while(n > maxKey){
-                setFibonacci(++maxKey);
+            while (n >= fibonacciStore.size()) {
+                setFibonacci(fibonacciStore.size());
             }
-
             Fibonacci f = fibonacciStore.get(n);
             System.out.println(f.toString());
         }
         br.close();
     }
 
-    static void setFibonacci(int n){
-
-        if(!fibonacciStore.containsKey(n)){
-            Fibonacci lFibonacci = fibonacciStore.get(n-1);
-            Fibonacci rFibonacci = fibonacciStore.get(n-2);
-
+    static void setFibonacci(int n) {
+        if (!fibonacciStore.containsKey(n)) {
             Fibonacci fibonacci = new Fibonacci();
-            fibonacci.setZero(lFibonacci.getZero() + rFibonacci.getZero());
-            fibonacci.setOne(lFibonacci.getOne() + rFibonacci.getOne());
+            if (n == 0) {
+                fibonacci.setZero(1);
+            } else if (n == 1) {
+                fibonacci.setOne(1);
+            } else {
+                Fibonacci lFibonacci = fibonacciStore.get(n - 1);
+                Fibonacci rFibonacci = fibonacciStore.get(n - 2);
+
+                fibonacci.setZero(lFibonacci.getZero() + rFibonacci.getZero());
+                fibonacci.setOne(lFibonacci.getOne() + rFibonacci.getOne());
+            }
             fibonacciStore.put(n, fibonacci);
         }
     }
-
 }
 
 class Fibonacci {
