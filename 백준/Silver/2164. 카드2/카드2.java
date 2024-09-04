41ec219b086ba3581843043c58d117db71d563ed
BongSeok Sin <Stupic0713@gmail.com>
2024-09-04 13:06:20 +0900

    [Silver IV] Title: 카드2, Time: 100 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,3 +1,6 @@
+//규칙:       *       *               *                               *            
+//입력:   1	2	3	4	5	6	7	8	9	10	11	12	13	14	15	16
+//결과:   1	2	2	4	2	4	6	8	2	4	6	8	10	12	14	16
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
