80bc3008ef08086e8a89ec9674eec7c5ea436913
BongSeok Sin <Stupic0713@gmail.com>
2024-09-06 10:32:00 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1172 ms, Memory: 96576 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int size = Integer.parseInt(br.readLine());
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            String input = br.readLine();
+            list.add(input);
+        }
+
+        //기수정렬 사용
+        int idx = 2;
+        while ( idx > 0 ) {
+            idx--;
+            ArrayList<String>[] radix = new ArrayList[200001];  //-100,000 ≤ xi, yi ≤ 100,000
+            for(String item : list){
+                String[] split = item.split(" ");
+                if(radix[Integer.parseInt(split[idx]) + 100000 ] == null){
+                    radix[Integer.parseInt(split[idx]) + 100000] = new ArrayList<>();
+                }
+                radix[Integer.parseInt(split[idx]) + 100000 ].add(item);
+            }
+
+            list.clear();
+            for (ArrayList<String> strings : radix) {
+                if (strings != null) {
+                    list.addAll(strings);
+                }
+            }
+        }
+        for (String string : list) {
+            System.out.println(string);
+        }
+        
+        br.close();
+    }
+}
