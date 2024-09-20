83c5d18da68defd32f8b08ebc9002c62a700eccc
BongSeok Sin <Stupic0713@gmail.com>
2024-09-20 16:16:27 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder s = new StringBuilder();
+
+        for (int i = n; i > 0; i--) {
+            s.append(" ".repeat(Math.max(0, n - i)));
+            s.append("*".repeat(Math.max(0, 2 * (i - 1) + 1)));
+
+            if ( i != 1){
+                s.append('\n');
+            }
+        }
+
+        System.out.println(s);
+        br.close();
+    }
+
+}
