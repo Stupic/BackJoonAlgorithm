72831fbe71484b06e240a13175ca3cedc63205b3
BongSeok Sin <Stupic0713@gmail.com>
2024-12-09 13:19:27 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 116 ms, Memory: 15468 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        System.out.println(str.substring( length-5));
+        br.close();
+    }
+}
