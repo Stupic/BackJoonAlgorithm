cdba189a868e1d271716db83bb2ed53c644c8640
BongSeok Sin <Stupic0713@gmail.com>
2024-12-25 21:43:49 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("""
+                       _.-;;-._
+                '-..-'|   ||   |
+                '-..-'|_.-;;-._|
+                '-..-'|   ||   |
+                '-..-'|_.-''-._|""");
+    }
+}
