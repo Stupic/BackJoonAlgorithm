1746827aed369279bb0d4cfca52d8ad35cd45c8d
BongSeok Sin <Stupic0713@gmail.com>
2025-01-02 14:18:12 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .").append("\n");
+        sb.append("|  | _ | _. _ ._ _  _").append("\n");
+        sb.append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+}
