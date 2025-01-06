d5f71612cfe79f9b3a153b54c073b41983432cb4
BongSeok Sin <Stupic0713@gmail.com>
2025-01-06 17:13:12 +0900

    [Bronze III] Title: 줄 세기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int lineCnt = 0;
+        String line;
+        while ((line = br.readLine()) != null && !line.trim().isEmpty()) {
+            lineCnt++;
+        }
+        System.out.println(lineCnt);
+        br.close();
+    }
+}
