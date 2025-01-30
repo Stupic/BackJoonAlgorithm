024c759ce87b6dce15ea3f78cb173507d5b0d016
BongSeok Sin <Stupic0713@gmail.com>
2025-01-30 23:07:55 +0900

    [Bronze V] Title: Metronome, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double n = Double.parseDouble( br.readLine());
+        System.out.println(n/4);
+        br.close();
+    }
+}
