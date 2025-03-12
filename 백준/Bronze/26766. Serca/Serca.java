6be90250c63660a63fe493ddd24ddc77b612852c
BongSeok Sin <Stupic0713@gmail.com>
2025-03-12 10:39:23 +0900

    [Bronze V] Title: Serca, Time: 288 ms, Memory: 63120 KB -BaekjoonHub

--- 백준/Bronze/26766. Serca/Serca.java
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -7,18 +7,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         while (n-- > 0){
-            System.out.println(
-                    " @@@   @@@ \n" +
-                    "@   @ @   @\n" +
-                    "@    @    @\n" +
-                    "@         @\n" +
-                    " @       @ \n" +
-                    "  @     @  \n" +
-                    "   @   @   \n" +
-                    "    @ @    \n" +
-                    "     @     ");
+            sb.append(" @@@   @@@ \n")
+              .append("@   @ @   @\n")
+              .append("@    @    @\n")
+              .append("@         @\n")
+              .append(" @       @ \n")
+              .append("  @     @  \n")
+              .append("   @   @   \n")
+              .append("    @ @    \n")
+              .append("     @     \n");
         }
+        System.out.println(sb);
         br.close();
     }
 }
