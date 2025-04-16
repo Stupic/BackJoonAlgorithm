bef0b0bff8b579376e82867e892d8c98dab7cb80
BongSeok Sin <Stupic0713@gmail.com>
2025-04-16 09:18:07 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 100 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int cnt = 0;
+        String input;
+        while ((input = br.readLine()) != null && !input.trim().isEmpty()) {
+            cnt++;
+        }
+        System.out.println(cnt);
+
+        br.close();
+    }
+}
