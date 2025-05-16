b628ba2bb8106b73da7b03a02fdf5a3636442213
BongSeok Sin <Stupic0713@gmail.com>
2025-05-16 15:03:51 +0900

    [Bronze V] Title: 제리와 톰, Time: 120 ms, Memory: 16024 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        System.out.println((m - n) + " " + m);
+
+        br.close();
+    }
+}
