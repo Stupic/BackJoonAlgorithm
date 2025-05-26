e82bb74173f3d9cb2a63b77d4a5713c2d868ab07
BongSeok Sin <Stupic0713@gmail.com>
2025-05-26 14:18:28 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int total = a + b + c + d;
+        System.out.println(total / 60);
+        System.out.println(total % 60);
+
+        br.close();
+    }
+}
