74128e8f18400d3c586f3c1be5b621db279edae7
BongSeok Sin <Stupic0713@gmail.com>
2025-06-02 10:07:05 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 104 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int a = Integer.parseInt(input[0]);
+        int b = Integer.parseInt(input[1]);
+        int c = Integer.parseInt(br.readLine());
+
+        System.out.println(a + b >= c * 2 ? a + b - c * 2 : a + b);
+        br.close();
+    }
+}
